1007beb34221a07299949089e310fdd4be3df8e8
Oskar Pfeifer-Bley <pfeifer-bley@ipa-dguv.de>
2016-09-09 09:25:45 +0200

    [[FEAT]] Add waterfall navigation effect

---
+++ js/waterfall-nav.js
@@ -0,0 +1,33 @@
+(function($) {
+    $.waterfallNav = function (selector, options) {
+        "use strict";
+        var didScroll = false,
+            defaults = {
+                scrollOffset: 50 // offset - 200 allows elements near bottom of page to scroll
+            };
+        options = $.extend(defaults, options);
+        selector = $(selector);
+
+        window.addEventListener("scroll", function() {
+            didScroll = true;
+        });
+
+        // Rate limit to 100ms
+        setInterval(function() {
+            if (! didScroll) {
+                return;
+            }
+            didScroll = false;
+
+            if (window.pageYOffset > options.scrollOffset) {
+                selector.addClass('nav--waterfall-shrunk');
+            } else {
+                selector.removeClass('nav--waterfall-shrunk');
+            }
+        }, 100);
+    };
+    $.fn.waterfallNav = function (offset) {
+        "use strict";
+        return $.waterfallNav($(this), offset);
+    };
+})(jQuery);
