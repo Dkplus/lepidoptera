85d42870a5a0025d37a7924e5c485df08e583181
Travis CI <oskar@programming-php.net>
2016-09-27 13:19:15 +0000

    Deploy to GitHub Pages: 6dbb750bf829c6f5cf33de082db8d3732500587d

--- kss-assets/waterfall-nav.js
+++ kss-assets/waterfall-nav.js
@@ -3,17 +3,28 @@
         "use strict";
         var defaults = {
                 scrollOffset: 50 // offset - 200 allows elements near bottom of page to scroll
-            };
+            },
+            listener,
+            lastPosition;
         options = $.extend(defaults, options);
         selector = $(selector);
-
-        window.addEventListener("scroll", function() {
-            if (window.pageYOffset > options.scrollOffset) {
+        lastPosition = 0;
+        listener = function() {
+            var isScrollUp = lastPosition - 5 > window.pageYOffset || (lastPosition > window.pageYOffset && window.pageYOffset < options.scrollOffset),
+                isScrollDown = lastPosition + 5 < window.pageYOffset || (lastPosition < window.pageYOffset && window.pageYOffset < options.scrollOffset);
+            if (isScrollDown) {
                 selector.addClass('navbar--waterfall-shrunk');
-            } else {
+            } else if(isScrollUp) {
                 selector.removeClass('navbar--waterfall-shrunk');
             }
-        });
+            lastPosition = window.pageYOffset;
+        };
+
+        listener();
+        window.addEventListener("scroll", listener);
+        /*$(window).on('scroll-up', function () {
+            selector.removeClass('navbar--waterfall-shrunk');
+        });*/
     };
     $.fn.waterfallNav = function (offset) {
         "use strict";
